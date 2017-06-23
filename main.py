cc36ff126f3f2fb35233e14049e82acd4234d16d
Ronnie Boelema <rboelema@gmail.com>
2017-06-23 15:24:10 +1200

    add brain functionality

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import yaml
 import speech_recognition as sr
 
+from brain import brain
 from GreyMatter.SenseCells.tts import tts
 
 profile = open('profile.yaml')
@@ -31,6 +32,6 @@ def main():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
     
-    tts(speech_text)
+    brain(name, speech_text)
     
 main()
